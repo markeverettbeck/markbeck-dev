05b77021392a8ba76a598dbf9767f9a96052796a
Matt Mitchell <mjmitchell86@gmail.com>
2020-06-09 12:33:52 -0400

    experience spelling mistakes

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -7,7 +7,7 @@
         <div class="resume-content">
           <h3 class="mb-0">Senior Software Engineer</h3>
           <div class="subheading mb-3">Rock Central LLC / Quicken Loans Inc.</div>
-          <p>As a Senior Software Engineer, my focus shifted from not just building and supporting software to also designing, architecting and educating. I became more involved with the Technology Standards conversations, designed larger scale applictions and mentored more junior and mid-level engineers within the company. Some of the projects I had the privledge of working on are:</p>
+          <p>As a Senior Software Engineer, my focus shifted from not just building and supporting software to also designing, architecting and educating. I became more involved with the Technology Standards conversations, designed larger scale applications and mentored more junior and mid-level engineers within the company. Some of the projects I had the privilege of working on are:</p>
           <p
             class="experience"
           >Designed and Built an interface for all Team Member data to be used by all internal applications. This was a hybrid on-prem/cloud solution that leveraged AWS Database Migration Service, MSSQL, AWS Aurora(MySQL), AWS Lambda, AWS API Gateway, Apollo GraphQL and the Serverless Framework.</p>
@@ -42,7 +42,7 @@
           <p>As a Software Engineer my focus was primarily on growing my skillset and helping provide valuable business solutions. During this time I had the pleasure of being involved in some innovative projects, some of which were:</p>
           <p
             class="experience"
-          >Worked on and maintained an internal tool for managing parking space allocation with reporting for all team members within the Family of Companies. This was a hyrbid on-prem/cloud solution that included a .Net Framework / Razor &amp; AngularJS UI with .Net Core REST API hosted on AWS Lambda using the AWS API Gateway and the Serverless Framework.</p>
+          >Worked on and maintained an internal tool for managing parking space allocation with reporting for all team members within the Family of Companies. This was a hybrid on-prem/cloud solution that included a .Net Framework / Razor &amp; AngularJS UI with .Net Core REST API hosted on AWS Lambda using the AWS API Gateway and the Serverless Framework.</p>
           <p
             class="experience"
           >Worked on and maintained retail leasing platform for Bedrock. This solution was a .Net Framework application with a Razor UI.</p>
