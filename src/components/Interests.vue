27b67fc05eafdbb8e07f877d1e6989e245cba98c
Matt Mitchell <mjmitchell86@gmail.com>
2020-06-12 20:03:33 -0400

    update kids to children

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -2,7 +2,7 @@
   <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
     <div class="w-100">
       <h2 class="mb-5">Interests</h2>
-      <p>Apart from being an engineer, my family recently grew by one. I enjoy spending time with family and being a Dad to two beautiful kids.</p>
+      <p>Apart from being an engineer, my family recently grew by one. I enjoy spending time with family and being a Dad to two beautiful children.</p>
       <p>
         In the summer you can usually find me somewhere near our small koi pond outside. For days that are too hot, I may have made a website so that I can watch my Koi from the comfort of... anywhere. Feel free to
         <a
