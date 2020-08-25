2a81401c34b72680df798c0b059bb008a756a126
Mark Beck <markeverettbeck@gmail.com>
2020-08-24 22:58:15 -0400

    Update Interests.vue
    Fixing spelling

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -2,7 +2,7 @@
   <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
     <div class="w-100">
       <h2 class="mb-5">Interests</h2>
-      <p>Apart from being an engineer, I enjoy traveling the world and learning to do new things that peek my interests.</p>
+      <p>Apart from being an engineer, I enjoy traveling the world and learning to do new things that pique my interests.</p>
       <p>
         I love to take photos, make music, and go on adventures. Some snipits of adventures can be found on instagram, feel free to 
         <a
